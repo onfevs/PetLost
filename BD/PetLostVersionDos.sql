eab97212ab7b2c4a77565b20cf8ee829626c6296
CACV00 <cacastro6482@misena.edu.co>
2022-09-07 20:08:03 -0500

    versionUno

---
+++ BD/PetLostVersionDos.sql
@@ -0,0 +1,50 @@
+DROP DATABASE PETLOST;
+CREATE DATABASE PETLOST;
+USE PETLOST;
+
+/*------Tablas------*/
+CREATE TABLE Persona (
+idPersona INT PRIMARY KEY AUTO_INCREMENT,
+numDocumento BIGINT NOT NULL, 
+nombres VARCHAR (60) NOT NULL,
+apellidos VARCHAR (60) NOT NULL
+);
+
+CREATE TABLE Usuario (
+fk_idPersona INT NOT NULL UNIQUE,
+correo VARCHAR (80) NOT NULL,
+contraseña INT NOT NULL,
+FOREIGN KEY (fk_idPersona) REFERENCES Persona(idPersona) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+        
+CREATE TABLE Contacto(
+idContacto INT PRIMARY KEY AUTO_INCREMENT,
+direccion VARCHAR (60) NOT NULL,
+celular BIGINT  NOT NULL,
+fk_idPersona INT NOT NULL,
+FOREIGN KEY (fk_idPersona) REFERENCES Persona(idPersona) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE TipoMascota(
+idtipoMascota INT PRIMARY KEY AUTO_INCREMENT,
+descripcion VARCHAR (30) NOT NULL
+);
+
+CREATE TABLE Mascota (
+idMascota INT PRIMARY KEY AUTO_INCREMENT,
+fk_idPersona INT NOT NULL,
+Nombre VARCHAR (30) NOT NULL,
+fk_tipoMascota INT NOT NULL,
+fotografia BLOB NOT NULL,
+fechaperdida DATE NOT NULL,
+lugarperdida VARCHAR (80) NOT NULL,
+FOREIGN KEY (fk_tipoMascota) REFERENCES TipoMascota (idtipoMascota) ON DELETE RESTRICT ON UPDATE CASCADE,
+FOREIGN KEY (fk_idPersona) REFERENCES Persona (idPersona) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+/*------Datos------*/
+INSERT INTO TipoMascota (idtipoMascota, descripcion)
+VALUES
+(DEFAULT, "Gato"),
+(DEFAULT, "Perro"),
+(DEFAULT, "Conejo"),
+(DEFAULT, "Hamster");
