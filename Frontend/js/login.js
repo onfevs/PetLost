ce8e095567fd073e079fc22a7efe457514b0d1c6
jmontero22c <jesusda71.2@gmail.com>
2022-09-12 21:14:48 -0500

    JavaScript

---
+++ Frontend/js/login.js
@@ -0,0 +1,28 @@
+async function login(){
+    let data_person = {};
+    data_person.email = document.getElementById("email").value;
+    data_person.password = document.getElementById("pass").value;
+
+    console.log(data_person)
+    
+    const request_person = await fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data_person)
+    });
+
+    const answer = await request_person.json();
+
+    console.log("Answer: "+ answer)
+
+    if(answer == true){
+        localStorage.setItem("email", data_person.email)
+        localStorage.setItem("password", data_person.password)
+        window.location.href = "./html/home.html";
+    }else{
+        alert("El usuario no existe")
+    }
+}
