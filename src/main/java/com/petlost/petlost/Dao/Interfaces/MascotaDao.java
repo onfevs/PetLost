37e068c24741ec1a189079ff6630c967f1f7d686
OnfeVS <jorgevallejo2990@gmail.com>
2022-09-27 08:49:46 -0500

    Cargo Petlost Back and Front completo

--- src/main/java/com/petlost/petlost/Dao/Interfaces/MascotaDao.java
+++
@@ -1,9 +0,0 @@
-package com.petlost.petlost.Dao.Interfaces;
-
-import com.petlost.petlost.Models.Mascota;
-import java.util.List;
-
-public interface MascotaDao {
-    public List<Mascota> getPet();
-    public String createPet(Mascota pet);
-}
